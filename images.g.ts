ffad8c9352920188150ddb515d166dd539c3035a
PandaBoi_7622 <125606723+PandaBoi7622@users.noreply.github.com>
2023-02-18 13:34:51 -0700

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Purple Guy":return img`
+. . . . . e 5 e e e e . . . . . 
+. . . . . e 5 e 5 e e e e . . . 
+. . . . . c c c c c c c . . . . 
+. . . . c c f c c f c c . . . . 
+. . . . c c f c c f c c . . . . 
+. c . . c c c c c c c c . . . . 
+c c . . . c f c c f c . . . . . 
+c c c . . c c f f c c . b . . . 
+. c c c c c c c c c c c . b b b 
+. . c c c c c c f c c c c b c . 
+. . . . c c c f f f c c . c . . 
+. . . . . . c c f c . . . . . . 
+. . . . . . c f f f . . . . . . 
+. . . . . . c c f c . . . . . . 
+. . . . . . c c c c . . . . . . 
+. . . . c c c . . c c c . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Purple Guy`, SpriteKind.Player))
 music.play(music.stringPlayable("C5 B A B G D C E ", 120), music.PlaybackMode.UntilDone)

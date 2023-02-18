1535638bc420f857e27e431f8b8a92e5dcd779f0
PandaBoi_7622 <125606723+PandaBoi7622@users.noreply.github.com>
2023-02-18 15:00:39 -0700

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,6 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "Purple Guy":return img`
-. . . . . e 5 e e e e . . . . . 
-. . . . . e 5 e 5 e e e e . . . 
-. . . . . c c c c c c c . . . . 
-. . . . c c f c c f c c . . . . 
-. . . . c c f c c f c c . . . . 
-. c . . c c c c c c c c . . . . 
-c c . . . c f c c f c . . . . . 
-c c c . . c c f f c c . b . . . 
-. c c c c c c c c c c c . b b b 
-. . c c c c c c f c c c c b c . 
-. . . . c c c f f f c c . c . . 
-. . . . . . c c f c . . . . . . 
-. . . . . . c f f f . . . . . . 
-. . . . . . c c f c . . . . . . 
-. . . . . . c c c c . . . . . . 
-. . . . c c c . . c c c . . . . 
-`;
             case "image2":
             case "Tank":return img`
 . . . . 6 6 6 6 6 6 . . . . . . 
@@ -54,6 +35,67 @@ d b f b f b f b f b f b d . . .
 5 2 d f a a 7 7 4 4 3 3 f e e 5 
 5 2 d d d a a 7 4 4 4 3 3 e e 5 
 2 2 d d d a 7 7 7 4 4 3 3 e 5 5 
+`;
+            case "image1":
+            case "Purple Guy":return img`
+. . . . . e 5 e e e e . . . . . 
+. . . . . e 5 e 5 e e e e . . . 
+. . . . . c c c c c c c . . . . 
+. . . . c c f c c f c c . . . . 
+. . . . c c f c c f c c . . . . 
+. . . . c c c c c c c c . . . . 
+. . . . . c f c c f c . . . . . 
+. . . b . c c f f c c . b . . . 
+b b b . c c c c c c c c . b b b 
+. c b c c c c c f c c c c b c . 
+. . c . c c c f f f c c . c . . 
+. . . . . . c c f c . . . . . . 
+. . . . . . c f f f . . . . . . 
+. . . . . . c c f c . . . . . . 
+. . . . . . c c c c . . . . . . 
+. . . . c c c . . c c c . . . . 
+`;
+            case "image5":
+            case "Bomb":return img`
+. . 5 . . . . . . . . 
+. 5 4 5 . . . . . . . 
+. 5 2 5 . . . . . . . 
+. d 1 d d . . . . . . 
+. . d 1 1 d . . . . . 
+. . b d 1 d b b b . . 
+. b c c f f f c c b . 
+b c c f 1 1 1 f c c b 
+b c f 1 2 1 2 1 f c b 
+b f f 1 2 1 2 1 f f b 
+b f f 1 1 1 1 1 f f b 
+b f f f 1 f 1 f f f b 
+b c f f f 1 f f f c b 
+b c c f f f f f c c b 
+. b c c f f f c c b . 
+. . b b b b b b b . . 
+`;
+            case "image4":
+            case "Nine_Eleven_Nuke":return img`
+.1111111111.........
+11fffffffff1........
+1ff55555555f1.......
+1f5525255255f11.....
+1f55225525255ff1....
+1f5525252225225f1...
+1f55252525255555f1..
+1f555555555555555f1.
+1f5555552555555555f1
+1f2255222225255525f1
+1f2525252525225225f1
+1f2225252525252525f1
+1f252522222525252f1.
+1f22555525555555f1..
+1f5555555555555f1...
+1f55555555555ff1....
+1f5555555555f11.....
+1ff55555555f1.......
+11fffffffff1........
+.1111111111.........
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,3 +1,156 @@
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
+    if (mp.playerSelector(mp.PlayerNumber.One) == player2) {
+        Purple_Guy_Right_Shot = sprites.createProjectileFromSprite(img`
+            f f 1 1 . . 
+            5 5 f f 1 . 
+            5 5 5 5 f 1 
+            5 5 f f 1 . 
+            f f 1 1 . . 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), 80, -25)
+        Purple_Guy_Left_Shot = sprites.createProjectileFromSprite(img`
+            . . 1 1 f f 
+            . 1 f f 5 5 
+            1 f 5 5 5 5 
+            . 1 f f 5 5 
+            . . 1 1 f f 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)), -80, -25)
+        pause(1000)
+    }
+    if (mp.playerSelector(mp.PlayerNumber.Two) == player2) {
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 25, 25)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), -25, -25)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 25, -25)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), -25, 25)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), -25, 0)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 25, 0)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 0, 25)
+        Glitch = sprites.createProjectileFromSprite(img`
+            2 a 2 3 9 4 
+            5 9 e a 2 9 
+            4 3 4 5 3 a 
+            3 a e 9 2 4 
+            a 2 2 a 9 e 
+            5 9 4 3 a 2 
+            `, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)), 0, -25)
+        pause(3000)
+    }
+    if (mp.playerSelector(mp.PlayerNumber.Three) == player2) {
+        Bomb = sprites.createProjectileFromSprite(assets.image`Bomb`, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)), 0, 75)
+        pause(500)
+    }
+    if (mp.playerSelector(mp.PlayerNumber.Four) == player2) {
+        Tank_Shot = sprites.createProjectileFromSprite(assets.image`Nine_Eleven_Nuke`, mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)), 150, 0)
+        pause(5000)
+    }
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    if (Bomb == sprite) {
+        sprites.destroy(sprite, effects.fire, 2000)
+    }
+    if (Glitch == sprite) {
+        sprites.destroy(sprite, effects.disintegrate, 5000)
+    }
+    if (Purple_Guy_Right_Shot == sprite) {
+        Purple_Guy_WarpHole_Right = sprites.createProjectileFromSprite(img`
+            . . . b b b b b b b b b b . . . 
+            . . b c c c c c c c c c c b . . 
+            . b c f f f f f f f f f f c b . 
+            b c f f f f a a a a a f f f c b 
+            b c f f f a f f f f a a f f c b 
+            b c f f a f f a a a f a a f c b 
+            b c f f a f a a f a a f a f c b 
+            b c f f a f a f f f a f a f c b 
+            b c f f a f a f a a f a f f c b 
+            b c f f a f a a a a f a f f c b 
+            b c f f a f f a a f f a f f c b 
+            b c f f f a a a a a a f f f c b 
+            b c f f f f f f f f f f f f c b 
+            . b c f f f f f f f f f f c b . 
+            . . b c c c c c c c c c c b . . 
+            . . . b b b b b b b b b b . . . 
+            `, Purple_Guy_Right_Shot, 0, 0)
+        sprites.destroy(sprite, effects.none, 5000)
+        sprites.destroy(Purple_Guy_WarpHole_Right, effects.starField, 5000)
+    }
+    if (Purple_Guy_Left_Shot == sprite) {
+        Purple_Guy_WarpHole_Left = sprites.createProjectileFromSprite(img`
+            . . . b b b b b b b b b b . . . 
+            . . b c c c c c c c c c c b . . 
+            . b c f f f f f f f f f f c b . 
+            b c f f f f a a a a a f f f c b 
+            b c f f f a f f f f a a f f c b 
+            b c f f a f f a a a f a a f c b 
+            b c f f a f a a f a a f a f c b 
+            b c f f a f a f f f a f a f c b 
+            b c f f a f a f a a f a f f c b 
+            b c f f a f a a a a f a f f c b 
+            b c f f a f f a a f f a f f c b 
+            b c f f f a a a a a a f f f c b 
+            b c f f f f f f f f f f f f c b 
+            . b c f f f f f f f f f f c b . 
+            . . b c c c c c c c c c c b . . 
+            . . . b b b b b b b b b b . . . 
+            `, Purple_Guy_Left_Shot, 0, 0)
+        sprites.destroy(sprite, effects.none, 5000)
+        sprites.destroy(Purple_Guy_WarpHole_Left, effects.starField, 100)
+    }
+})
+let Purple_Guy_WarpHole_Left: Sprite = null
+let Purple_Guy_WarpHole_Right: Sprite = null
+let Tank_Shot: Sprite = null
+let Bomb: Sprite = null
+let Glitch: Sprite = null
+let Purple_Guy_Left_Shot: Sprite = null
+let Purple_Guy_Right_Shot: Sprite = null
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Purple Guy`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Error_Block`, SpriteKind.Player))
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(img`
